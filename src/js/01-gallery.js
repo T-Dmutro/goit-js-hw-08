67af683c23b20a3d6bd87c8206dae627fd695ac1
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-17 21:00:22 +0200

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(galleryItems);
+const cartContainer = document.querySelector('.gallery');
+// console.log(cartContainer)
+cartContainer.addEventListener('click', onCardsContainerClick)
+const cardsMarkup = createCardMarkup(galleryItems);
+cartContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createCardMarkup( element){ 
+    return element.map(({preview, original, description})=>{
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    })
+    .join(" ");}
+    
+// }console.log(createCardMarkup)
+const instance =new SimpleLightbox('.gallery a', { sourceAttr : "href"});
+lightbox.on("show.simplelightbox");
+function onCardsContainerClick(el){
+    // console.log(el.target.parentNode)
+    el.preventDefault()
+ if (el.target.nodeName !== 'IMG'){
+    return;
+ }
+//  console.log(el.target.dataset.source)
+
+    instance.show();
+   
+    window.addEventListener("keydown", (el)=>{
+    if (el.code === "Escape") {
+        instance.close();}
+    })
+ 
+}
+
+// function blockStandartAction(el){
+//     el.preventDefault()
+// }
