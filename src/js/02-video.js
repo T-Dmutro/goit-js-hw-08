3b6135875e7386c56910d7ca3c0e02db612dab1f
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-18 11:35:53 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,10 @@
 import Player from '@vimeo/player';
-import  throttle from "lodash.throttle";
+
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCALPLAYER_KEY = 'videoplayer-current-time';
 
 
 
-function onPlay(data) {
-    const timePlayer = data.seconds;
-    localStorage.setItem(LOCALPLAYER_KEY, timePlayer);
-}
 
-player.on('timeupdate', throttle(onPlay, 1000));
-console.log(onPlay);
-
-const saveTime = localStorage.getItem(LOCALPLAYER_KEY);
-  if(saveTime){
-    player.setCurrentTime(saveTime);
-  }
-
-player.on('play', throttle(saveTime, 1000));
